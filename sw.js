a20699d48a77696be75ae79747b2e1f87a7b308f
manassentambwa99-ui <manassentambwa99@gmail.com>
2026-09-23 22:17:01 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,13 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('jorvie-store').then((cache) => cache.addAll([
-      './index.html',
-      './manifest.json',
-      './logo.svg'
-    ])),
-  );
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request)),
-  );
-});
-self.addEventListener('install', (e) => {
+hereself.addEventListener('install', (event) => {
   self.skipWaiting();
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
 });
